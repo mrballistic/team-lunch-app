d82284010df349bdb248165ab4737c7bb981f7d3
Todd Greco <mrballistic@gmail.com>
2025-09-10 09:40:10 -0700

    fix: clean up comments and improve readability in jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,16 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+/* eslint-disable @typescript-eslint/no-require-imports */
 require('dotenv').config({ path: '.env.test' });
-// Polyfill fetch in the test environment
+
+// Polyfill fetch in the test environment (optional)
 try {
-	// eslint-disable-next-line global-require
 	require('cross-fetch/polyfill');
-} catch (_) {
-	// optional
-}
-// Enable DOM matchers for testing-library
+} catch {}
+
+// Enable DOM matchers for testing-library (optional)
 try {
-	// eslint-disable-next-line global-require
 	require('@testing-library/jest-dom');
-} catch (_) {
-	// optional
-}
+} catch {}
